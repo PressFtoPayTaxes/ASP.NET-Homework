2119e020c58dced7c938591b0557e255329ae482
Игорь Скоропад <igor_2002_25@mail.ru>
2019-10-30 18:34:45 +0600

    New Solution

---
+++ AspNetMvcDotNetFramework/AspNetMvcDotNetFramework1/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+
+namespace AspNetMvcDotNetFramework1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendEmail(string email)
+        {
+            SmtpClient client = new SmtpClient("localhost", 2222);
+            client.Send("igor_2002_25@mail.ru", email, "Subscribtion", "Congratulations! You subscribed to OneTech shop!");
+
+            return Index();
+        }
+    }
+}
---
+++ AspNetMvcDotNetFramework/AspNetMvcDotNetFramework1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetMvcDotNetFramework1.MvcApplication" Language="C#" %>
